41f0d7813264749835884d150ac2b9c99f839877
Nayoung-apeach <s2019w04@e-mirim.hs.kr>
2020-11-17 20:24:33 +0900

    Complete Project2 :D

--- app/src/main/java/kr/hs/emirim/s2019w04/mirimtoastdialogtest/MainActivity2.java
+++ app/src/main/java/kr/hs/emirim/s2019w04/mirimtoastdialogtest/MainActivity2.java
@@ -5,33 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
-    EditText editName, editEmail; //activity_main
-    TextView textToast; //toast1
+    EditText editMainName, editMainEmail;
+    EditText editName, editEmail;
+    TextView textToast;
     View dialogView, toastView;
-    Button btnMainHere; //activity_main
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        editName = findViewById(R.id.edit_main_name);
-        editEmail = findViewById(R.id.edit_main_email);
-        btnMainHere = findViewById(R.id.btn_main_here);
-        btnMainHere.setOnClickListener(btnHereListener);
+        setContentView(R.layout.activity_main2);
+        editMainName = findViewById(R.id.edit_main_name);
+        editMainEmail = findViewById(R.id.edit_main_email);
+        Button btnHere = findViewById(R.id.btn_here2);
+        btnHere.setOnClickListener(btnHereListener);
     }
+
     View.OnClickListener btnHereListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             dialogView = View.inflate(MainActivity2.this, R.layout.dialog1, null);
-            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity2.this);
-            dlg.setTitle("사용자 정보 입력");
+            AlertDialog.Builder dlg =new AlertDialog.Builder(MainActivity2.this);
+            dlg.setTitle("학생 정보 입력");
             dlg.setIcon(R.drawable.heart);
             dlg.setView(dialogView);
             dlg.setPositiveButton("확인", positiveButtonListener);
@@ -39,13 +43,14 @@ public class MainActivity2 extends AppCompatActivity {
             dlg.show();
         }
     };
+
     DialogInterface.OnClickListener positiveButtonListener = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialog, int which) {
             editName = dialogView.findViewById(R.id.edit_name);
             editEmail = dialogView.findViewById(R.id.edit_email);
-            editName.setText(editName.getText().toString() + "님 입니다.");
-            editEmail.setText(editEmail.getText().toString() + "이메일 주소입니다.");
+            editMainName.setText(editName.getText().toString());
+            editMainEmail.setText(editEmail.getText().toString());
         }
     };
 
@@ -54,10 +59,13 @@ public class MainActivity2 extends AppCompatActivity {
         public void onClick(DialogInterface dialog, int which) {
             Toast toast = new Toast(MainActivity2.this);
             toastView = View.inflate(MainActivity2.this, R.layout.toast1, null);
-            toast.setView(toastView);
             textToast = toastView.findViewById(R.id.text_toast);
             textToast.setText("취소버튼을 누르셨네요.");
             toast.setView(toastView);
+            Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+            int xOffset = (int) (Math.random() * display.getWidth());
+            int yOffset = (int) (Math.random() * display.getHeight());
+            toast.setGravity(Gravity.TOP | Gravity.LEFT, xOffset, yOffset);
             toast.show();
         }
     };
